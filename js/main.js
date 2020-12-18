6d229cd52708a161ed3c74571612c8ac24ecf6a8
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-19 00:39:15 +0900

    create main loop (on the way)

--- js/main.js
+++ js/main.js
@@ -8,20 +8,43 @@
   //canvasの描画準備//
   let can = document.getElementById("can");
   let con = can.getContext("2d");
+  let frameCount = 0;
 
   //画面の生成//
   can.width = SCREEN_SIZE_WIDTH;
   can.height = SCREEN_SIZE_HEIGHT;
-  con.fillStyle = "#66AAFF";
-  con.fillRect(0, 0, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT);
-
+  
   //キャラクターの生成//
   let chImg = new Image();
   chImg.src = "sprite.png";
   chImg.onload = draw;
+  
+
+  function update(){
+  }
+  
+  //画面描画処理//
   function draw(){
-    con.drawImage(chImg,  16, 0, 16, 32,  0, 0, 16, 32);
+    //背景
+    con.fillStyle = "#66AAFF";
+    con.fillRect(0, 0, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT);
+    //キャラクター
+    con.drawImage(chImg,  0, 48, 16, 32,  16, 32, 16, 32);
     //drawImageメソッド
+    //前半が描画の始点とそこからの描画読み取り範囲
+    //後半がcanvasの開始地点とcanvasの表示領域
+    frameCount++;
+    con.fillStyle ="white";
+    con.fillText("Frame:"+frameCount,10,10)
   }
 
+  //メインループ//
+  function mainLoop(){
+    update();
+    draw();
+    // setTimeout(mainLoop,1000/60);
+  }
+
+  setInterval(mainLoop,1000/60);
+  // mainLoop();
 }
