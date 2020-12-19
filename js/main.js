8ec484d5b594c5ff5047bd63e4ba2c0f5dafcea4
ToshifumiIde <toshifumiide198765@gmail.com>
2020-12-19 10:23:39 +0900

    virtual canvas create control magnification by virtual canvas

--- js/main.js
+++ js/main.js
@@ -10,14 +10,20 @@
   let can = document.getElementById("can");
   let con = can.getContext("2d");
 
+  //virtualCanvasの生成（倍率制御）
+  let vcan = document.createElement("canvas");
+  let vcon = vcan.getContext("2d");
+
   //時間管理の変数//
   let frameCount = 0;//画面上のタイマー
   let startTime = 0;//requestAnimationFrame()の正確な更新を制御する
 
 
   //画面の生成//
-  can.width = SCREEN_SIZE_WIDTH;
-  can.height = SCREEN_SIZE_HEIGHT;
+  vcan.width  = SCREEN_SIZE_WIDTH;
+  vcan.height = SCREEN_SIZE_HEIGHT;
+  can.width  = SCREEN_SIZE_WIDTH * 2;
+  can.height = SCREEN_SIZE_HEIGHT* 2;
   
   //キャラクターの生成//
   let chImg = new Image();
@@ -31,16 +37,17 @@
   //画面描画処理//
   function draw(){
     //背景
-    con.fillStyle = "#66AAFF";
-    con.fillRect(0, 0, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT);
+    vcon.fillStyle = "#66AAFF";
+    vcon.fillRect(0, 0, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT);
     //キャラクター
-    con.drawImage(chImg,  80, 0, 16, 32,  16, 32, 16, 32);
+    vcon.drawImage(chImg,  80, 0, 16, 32,  16, 32, 16, 32);
     //drawImageメソッド
     //前半が描画の始点と、始点からの描画読み取り範囲
     //後半がcanvasの開始地点とcanvasの表示領域
-    con.font = "16px 'Impact'";
-    con.fillStyle ="white";
-    con.fillText(`Frame:${frameCount}`,10,20)
+    vcon.font = "16px 'Impact'";
+    vcon.fillStyle ="white";
+    vcon.fillText(`Frame:${frameCount}`,10,20);
+    con.drawImage(vcan, 0,0,SCREEN_SIZE_WIDTH,SCREEN_SIZE_HEIGHT ,0,0,SCREEN_SIZE_WIDTH*3,SCREEN_SIZE_HEIGHT*3);
   }
 
   //メインループ//
@@ -52,7 +59,7 @@
       //処理落ちした際に一定時間で更新を止める
       let count = 0;
       while(nowFrame > frameCount){
-        // count ++;
+        count ++;
         frameCount++;
         update();
         if(++count >= 4)break;
